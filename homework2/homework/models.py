2b44a60111e474f65a22c445255471da9da635b7
Xin <edi.wangxin@gmail.com>
2023-09-02 22:48:32 -0400

    add blocks to cnn

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -2,16 +2,32 @@ import torch
 
 
 class CNNClassifier(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+                torch.nn.ReLU()
+            )
+
+        def forward(self, x):
+            return self.net(x)
+
     def __init__(self):
         super().__init__()
         layer_channel_counts = [32, 64, 128]
-        L= []
-        input_channel = 3
+        L = [torch.nn.Conv2d(3, 32, kernel_size=7, padding=3, stride=2),
+             torch.nn.ReLU(),
+             torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)]
 
+        input_channel = 32
         for output_channel in layer_channel_counts:
-            L.append(torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel,
-                                     kernel_size=3, padding=1, stride=2))
-            L.append(torch.nn.ReLU())
+            L.append(self.Block(input_channel, output_channel, stride=2))
+            # L.append(torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel,
+            #                          kernel_size=3, padding=1, stride=2))
+            # L.append(torch.nn.ReLU())
             input_channel = output_channel
         self.network = torch.nn.Sequential(*L)
         self.classifier = torch.nn.Linear(in_features=input_channel, out_features=6)

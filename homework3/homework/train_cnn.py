a6c1e2dfce1ff3e544c6f04b8e489b39157016ac
Xin <edi.wangxin@gmail.com>
2023-09-19 11:33:50 -0400

    perforn random erase for training data

--- homework3/homework/train_cnn.py
+++ homework3/homework/train_cnn.py
@@ -25,7 +25,7 @@ def train(args):
     loss = torch.nn.CrossEntropyLoss()
 
     # data loader
-    train_data_loader = load_data('data/train', batch_size=batch_size, random_crop=(60,60), flip=True, color_aug=True)
+    train_data_loader = load_data('data/train', batch_size=batch_size, random_crop=(60,60), flip=True, color_aug=True, random_erase=True)
     valid_data_loader = load_data('data/valid', batch_size=batch_size, resize=(60,60))
 
     # optimizer

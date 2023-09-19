d77878235d1a5833b7d50854a85de74ff1d26c2b
Xin <edi.wangxin@gmail.com>
2023-09-19 18:20:20 -0400

    FCN model

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -48,8 +48,30 @@ class CNNClassifier(torch.nn.Module):
 
 
 class FCN(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+                torch.nn.BatchNorm2d(n_output),
+                torch.nn.ReLU()
+            )
+            self.downsample = None
+            if stride != 1 or n_input != n_output:
+                self.downsample = torch.nn.Sequential(torch.nn.Conv2d(n_input, n_output, 1, stride=stride),
+                                                      torch.nn.BatchNorm2d(n_output))
+
+        def forward(self, x):
+            identity = x
+            if self.downsample is not None:
+                identity = self.downsample(x)
+            return self.net(x)+identity    
+            
     def __init__(self):
-        super().__init__()
+        super().__init__(self, n_input_channels=3)
         """
         Your code here.
         Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
@@ -58,7 +80,20 @@ class FCN(torch.nn.Module):
         Hint: Use residual connections
         Hint: Always pad by kernel_size / 2, use an odd kernel_size
         """
-        raise NotImplementedError('FCN.__init__')
+        # Initial convolutional layer
+        self.conv1 = torch.nn.Conv2d(n_input_channels, 64, kernel_size=3, padding=1)
+        self.relu = torch.nn.ReLU()
+
+        # blocks
+        self.block1 = Block(64, 128, stride=2)
+        self.block2 = Block(128, 256, stride=2)
+
+        # up-convolutionn
+        self.upconv1 = torch.nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1)
+        self.upconv2 = torch.nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1)
+
+        # final convo layer
+        self.output_layer = nn.Conv2d(64, 5, kernel_size=1)
 
     def forward(self, x):
         """
@@ -70,7 +105,22 @@ class FCN(torch.nn.Module):
               if required (use z = z[:, :, :H, :W], where H and W are the height and width of a corresponding strided
               convolution
         """
-        raise NotImplementedError('FCN.forward')
+         # through initial layer
+        x1 = self.conv1(x)
+        x1 = self.relu(x1)
+
+        # through blocks
+        x2 = self.block1(x1)
+        x3 = self.block2(x2)
+
+        # up-connvo
+        x4 = self.upconv1(x3)
+        # skip connection
+        x4 = torch.cat([x4, x2], dim=1)  
+        
+        x5 = self.upconv2(x4)
+        x5 = torch.cat([x5, x1], dim=1)  # Concatenate skip connection
+        
 
 
 model_factory = {

e9324c4a133c4bc5aec7724b6f1d3f2eeaa9386e
Xin <edi.wangxin@gmail.com>
2023-09-20 00:08:15 -0400

    update data_scan to get stats for dense_data

--- homework3/homework/data_scan.py
+++ homework3/homework/data_scan.py
@@ -2,7 +2,7 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from .utils import SuperTuxDataset
+from .utils import SuperTuxDataset, DenseSuperTuxDataset
 
 if __name__ == '__main__':
     train_data = SuperTuxDataset('data/train')
@@ -14,3 +14,13 @@ if __name__ == '__main__':
     std = torch.std(concat, dim=[0,2,3])
     print("mean: " + repr(mean))
     print("std:  " + repr(std))
+
+    dense_train_data = DenseSuperTuxDataset('dense_data/train')
+    images = []
+    for data, label in train_data:
+        images.append(data[None, :])
+    concat=torch.concat(images, dim=0)
+    mean = torch.mean(concat, dim=[0,2,3])
+    std = torch.std(concat, dim=[0,2,3])
+    print("dense mean: " + repr(mean))
+    print("dense std:  " + repr(std))

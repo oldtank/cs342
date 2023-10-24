e8f5e447aa9f04b1ba7ad7a84074e53a7c620310
Xin <edi.wangxin@gmail.com>
2023-10-24 18:30:47 -0400

    Update controller.py

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -9,7 +9,7 @@ def control(aim_point, current_vel):
     :return: a pystk.Action (set acceleration, brake, steer, drift)
     """
     action = pystk.Action()
-    target_velocity = 37
+    target_velocity = 40
 
     if current_vel > target_velocity:
         action.brake = True

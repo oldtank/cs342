af2e98e85d6a1c29d52743787df3b7cd1b184daa
Xin <edi.wangxin@gmail.com>
2023-10-24 17:19:55 -0400

    Update controller.py

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -33,7 +33,7 @@ def control(aim_point, current_vel):
     action.steer = steer_sign * min(steer_magnitude, 1)
     if steer_magnitude > 2:
         action.drift = True
-    if steer_magntitude > 10:
+    if steer_magnitude > 10:
         action.brake=True
     
     # if aim_point[0] < 0:

f4876aee251ca8f7f234f753d0b656011b8100e4
Xin <edi.wangxin@gmail.com>
2023-10-24 19:47:12 -0400

    Update controller.py

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -29,6 +29,8 @@ def control(aim_point, current_vel):
     if current_vel < target_velocity:
         if steer_magnitude < 10:
             action.acceleration = (target_velocity - current_vel) / target_velocity
+        else
+            action.acceleration = 0.1
     
 
     action.steer = steer_sign * min(1.25*steer_magnitude, 1)

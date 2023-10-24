1d8c3cee28a1eefbea7adad61fd415c9262c5dd5
Xin <edi.wangxin@gmail.com>
2023-10-24 16:06:14 -0400

    Update controller.py

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -27,7 +27,7 @@ def control(aim_point, current_vel):
             action.steer = 3.3*aim_point[0]
         else:
             action.steer = 1
-    if aim_point[0] > 0.6 or aim_point[0] < -0.6:
+    if aim_point[0] > 0.5 or aim_point[0] < -0.5:
         action.drift = True
 
     """

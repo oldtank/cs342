a0b9b47551de725269c4d0cb0f525a5527ee5e7a
Xin <edi.wangxin@gmail.com>
2023-11-05 00:35:28 -0400

    Update model.py

--- final/state_agent/model.py
+++ final/state_agent/model.py
@@ -5,30 +5,56 @@ class StateAgentModel(torch.nn.Module):
     def __init__(self, input_size):
         super().__init__()
 
-        self.conv1 =torch.nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        # self.conv1 =torch.nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        # self.relu1 = torch.nn.ReLU()
+        # self.pool1 = torch.nn.MaxPool1d(kernel_size=2)
+        #
+        # self.conv2 =torch.nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        # self.relu2 = torch.nn.ReLU()
+        # self.pool2 = torch.nn.MaxPool1d(kernel_size=2)
+        #
+        # self.fc1 = torch.nn.Linear(128, 256)
+        # self.relu3 = torch.nn.ReLU()
+        # self.fc2 = torch.nn.Linear(256, 64)
+        # self.relu4 = torch.nn.ReLU()
+        # self.fc3 = torch.nn.Linear(64, 3)
+
+        self.fc1 = torch.nn.Linear(11, 32)
         self.relu1 = torch.nn.ReLU()
-        self.pool1 = torch.nn.MaxPool1d(kernel_size=2)
-
-        self.conv2 =torch.nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.fc2 = torch.nn.Linear(32, 64)
         self.relu2 = torch.nn.ReLU()
-        self.pool2 = torch.nn.MaxPool1d(kernel_size=2)
-
-        self.fc1 = torch.nn.Linear(128, 256)
+        self.fc3 = torch.nn.Linear(64, 128)
         self.relu3 = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(256, 64)
+        self.fc4 = torch.nn.Linear(128, 64)
         self.relu4 = torch.nn.ReLU()
-        self.fc3 = torch.nn.Linear(64, 3)
+        self.fc5 = torch.nn.Linear(64, 32)
+        self.relu5 = torch.nn.ReLU()
+        self.fc6 = torch.nn.Linear(32, 3)
+
+        self.network = torch.nn.Sequential(
+            self.fc1,self.relu1,self.fc2,self.relu2,
+            self.fc3,
+            self.relu3,
+            self.fc4,
+            self.relu4,
+            self.fc5,
+            self.relu5,
+            self.fc6
+        )
 
     def forward(self, x):
-        # print(x.shape)
-        x = self.pool1(self.relu1(self.conv1(x)))
-        # print(x.shape)
-        x = self.pool2(self.relu2(self.conv2(x)))
-        x = x.view(x.size(0), -1)  # Flatten the output from convolutional layers
-        # print(x.shape)
-        x = self.relu3(self.fc1(x))
-        x = self.relu4(self.fc2(x))
-        x = self.fc3(x)
+        # # print(x.shape)
+        # x = self.pool1(self.relu1(self.conv1(x)))
+        # # print(x.shape)
+        # x = self.pool2(self.relu2(self.conv2(x)))
+        # x = x.view(x.size(0), -1)  # Flatten the output from convolutional layers
+        # # print(x.shape)
+        # x = self.relu3(self.fc1(x))
+        # x = self.relu4(self.fc2(x))
+        # x = self.fc3(x)
+
+        x = x.squeeze(1)
+        x= self.network(x)
         return x
 
 def save_model(model):

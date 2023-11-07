53cfe761602378e7b55c7b799c3799cfcd0da047
Xin <edi.wangxin@gmail.com>
2023-11-06 21:54:55 -0500

    Update player.py

--- final/state_agent/player.py
+++ final/state_agent/player.py
@@ -92,8 +92,8 @@ class Team:
         actions = []
         for player_id, pstate in enumerate(player_state):
             features = get_featuers_for_player(pstate, soccer_state, self.team)
-            print(features.shape)
-            acceleration, steer, brake = self.model(features.to(self.device))
+            # print(features.shape)
+            acceleration, steer, brake = self.model(features.to(self.device).unsqueeze(0).unsqueeze(0))
 
             acceleration_val = acceleration[0].item()
             steer_val = steer[0].item()

f914257d523ea56eca8b281be59815c2f33c8d1f
Xin <edi.wangxin@gmail.com>
2023-12-01 13:24:10 -0500

    Update model.py

--- final/state_agent/model.py
+++ final/state_agent/model.py
@@ -1,65 +1,34 @@
-import torch
+import torch 
 
 class StateAgentModel(torch.nn.Module):
-
-    def __init__(self, input_size):
+    def __init__(self, layer_sizes = [32, 64, 128, 256], n_input_features=11, n_output_features=3):
         super().__init__()
 
-        # self.conv1 =torch.nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
-        # self.relu1 = torch.nn.ReLU()
-        # self.pool1 = torch.nn.MaxPool1d(kernel_size=2)
-        #
-        # self.conv2 =torch.nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        # self.relu2 = torch.nn.ReLU()
-        # self.pool2 = torch.nn.MaxPool1d(kernel_size=2)
-        #
-        # self.fc1 = torch.nn.Linear(128, 256)
-        # self.relu3 = torch.nn.ReLU()
-        # self.fc2 = torch.nn.Linear(256, 64)
-        # self.relu4 = torch.nn.ReLU()
-        # self.fc3 = torch.nn.Linear(64, 3)
+        layers = []
+        layers.append(torch.nn.Linear(n_input_features, layer_sizes[0]))
+        for i in range(len(layer_sizes) - 1):
+            layers.append(torch.nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+            layers.append(torch.nn.LayerNorm(layer_sizes[i+1]))
+            layers.append(torch.nn.ReLU())
 
-        self.fc1 = torch.nn.Linear(11, 32)
-        self.relu1 = torch.nn.ReLU()
-        self.fc2 = torch.nn.Linear(32, 64)
-        self.relu2 = torch.nn.ReLU()
-        self.fc3 = torch.nn.Linear(64, 128)
+        self.network = torch.nn.Sequential(*layers)
 
-        self.fc4 = torch.nn.Linear(128, 64)
-        self.fc5 = torch.nn.Linear(64, 32)
+        self.classifier = torch.nn.Linear(layer_sizes[-1], n_output_features)
 
-        # Output layers
-        self.output1 = torch.nn.Linear(32, 1)  # First output (floating-point)
-        self.output2 = torch.nn.Linear(32, 1)  # Second output (floating-point)
-        self.output3 = torch.nn.Linear(32, 1)  # Third output (floating-point)
-
-        self.network = torch.nn.Sequential(
-            self.fc1,self.relu1,self.fc2,self.relu2,
-            self.fc3,
-            self.fc4,
-            self.fc5,
-        )
 
     def forward(self, x):
-        # # print(x.shape)
-        # x = self.pool1(self.relu1(self.conv1(x)))
-        # # print(x.shape)
-        # x = self.pool2(self.relu2(self.conv2(x)))
-        # x = x.view(x.size(0), -1)  # Flatten the output from convolutional layers
-        # # print(x.shape)
-        # x = self.relu3(self.fc1(x))
-        # x = self.relu4(self.fc2(x))
-        # x = self.fc3(x)
+        n = self.network(x)
+        z = self.classifier(n)
+        acceleration = torch.sigmoid(z[:,0])
+        steer = torch.tanh(z[:,1])
+        brake = torch.sigmoid(z[:,2])
 
-        x= self.network(x)
-
-        acceleration = torch.sigmoid(self.output1(x))
-        steer = torch.tanh(self.output2(x))
-        brake = torch.sigmoid(self.output3(x))
-
-        # brake_mask = brake > 0.5
-        # acceleration[brake_mask] = 0
-        return acceleration, steer, brake
+     
+        assert torch.all((acceleration >= 0) & (acceleration <= 1)), "Values for acceleration are out of range [0, 1]"
+        assert torch.all((steer >= -1) & (steer <= 1)), "Values for steer are out of range [-1, 1]"
+        assert torch.all((brake >= 0) & (brake <= 1)), "Values for brake are out of range [0, 1]"
+        
+        return acceleration.unsqueeze(1), steer.unsqueeze(1), brake.unsqueeze(1)
 
 def save_model(model):
     from torch import save

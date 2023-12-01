bcd62616f331637582b1b13c5fafcbc40a04020e
Xin <edi.wangxin@gmail.com>
2023-12-01 13:24:56 -0500

    Update train.py

--- final/state_agent/train.py
+++ final/state_agent/train.py
@@ -1,11 +1,9 @@
 import torch
 import numpy as np
-import torch.utils.tensorboard as tb
-from .utils import load_data
 from .model import StateAgentModel, save_model
 from os import path
-import math
-
+import wandb
+import pickle
 
 class CustomLoss(torch.nn.Module):
     def __init__(self, device):
@@ -15,39 +13,49 @@ class CustomLoss(torch.nn.Module):
         self.brake_loss = torch.nn.BCELoss().to(device)
 
     def forward(self, acceleration, steer, brake, batch_label):
-        # brake_mask = brake > 0.5
-        # acceleration[brake_mask] = 0
 
-        # brake_mask_label = batch_label[:,2] == 1
-
+        acceleration_loss_val = self.acceleration_loss(acceleration, batch_label[:, 0].unsqueeze(1))
         steer_loss_val = self.steer_loss(steer, batch_label[:, 1].unsqueeze(1))
         brake_loss_val = self.brake_loss(brake, batch_label[:, 2].unsqueeze(1))
-        acceleration_loss_val = self.acceleration_loss(acceleration, batch_label[:, 0].unsqueeze(1))
 
         return steer_loss_val + brake_loss_val + acceleration_loss_val
-
+    
 def train(args):
     from os import path
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    model = StateAgentModel().to(device)
+    model = StateAgentModel(n_input_features=11).to(device)
     if args.continue_training:
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'state_agent.th')))
+        # model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'state_agent.th')))
+        model = torch.jit.load(path.join(path.dirname(path.abspath(__file__)), 'state_agent.pt'))
+        train_data = pickle.load(open('train_data.pkl', 'rb'))
+    # else:
+    #     train_data = pickle.load(open('super_winners_only.pkl', 'rb'))
+    #     print("loaded pickle file datastet", len(train_data))
+
+    train_data = pickle.load(open('train_data.pkl', 'rb'))
+
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
 
     loss = CustomLoss(device)
 
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+    # train_data = load_data('recordings', num_workers=2, batch_size=args.batch_size)
 
-    train_data = load_data(num_workers=4,batch_size=32)
-    global_step = 1
+    wandb.init(
+    # set the wandb project where this run will be logged
+        project="final_project",
+        config = {
+            "epochs": args.epoch,
+            "optimizer": optimizer.__class__.__name__,
+            "device": device
+        }
+    )
 
+    global_step = 0
     model.train()
     for epoch in range(args.epoch):
-        print('starting epoch %d' % epoch)
+        loss_vals = []
+        print("EPOCH", epoch)
         for batch_data, batch_label in train_data:
             # print('train data: %s' % repr(train_data))
             # print('train label: %s' % repr(train_label))
@@ -60,20 +68,16 @@ def train(args):
 
             loss_val = loss(acceleration, steer, brake, batch_label)
 
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
-
             if global_step % 1000 == 0:
-                print('loss val % f' % loss_val)
+                # print('loss val % f' % loss_val)
+                wandb.log({"train/loss": loss_val}, step=global_step)
 
-            if math.isnan(loss_val.item()):
-                print('nan. label: %s' % (repr(batch_label)))
-                break
-            # print('loss val % f' % loss_val)
             optimizer.zero_grad()
             loss_val.backward()
             optimizer.step()
             global_step +=1
+            loss_vals.append(loss_val.item())
+        print("average loss for epoch", np.mean(loss_vals))
 
     save_model(model)
 
@@ -87,6 +91,7 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--epoch', default=20, type=int)
     parser.add_argument('-c', '--continue_training', default=False)
     parser.add_argument('-lr', '--learning_rate', default=1e-4, type=float)
+    parser.add_argument('-b', '--batch_size', default=32, type=int)
 
     args = parser.parse_args()
     train(args)
